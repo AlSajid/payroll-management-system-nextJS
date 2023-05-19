1829c88456ad66036b154741a9efd3b7f054e6de
Al Sajid <alsajiduzzaman@outlook.com>
2023-05-20 01:26:35 +0600

    primary button dropdown functionality added

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,37 @@
 import AddEmployee from "@/components/AddEmployee";
 import EmployeeList from "@/components/EmployeeList";
 import { useState } from "react";
+import { BsPersonPlusFill } from "react-icons/bs";
+import { SlClose } from "react-icons/sl";
 
 export default function Home() {
   const [isFormOpen, setIsFormOpen] = useState(false);
 
   return (
-    <main>
-      <button className="bg-yellow-300 rounded">Add New Employee</button>
+    <main className="container flex flex-col items-center">
+      {/* add employee button */}
+      <button
+        className="bg-yellow-300 hover:bg-yellow-400
+                  rounded p-3 m-3 hover:scale-105 transform transition
+                  shadow-sm text-lg font-semibold
+                  flex items-center justify-center gap-3"
+        onClick={() => setIsFormOpen(!isFormOpen)}
+      >
+        {isFormOpen ? (
+          <>
+            <SlClose />
+            <span>Close the form</span>
+          </>
+        ) : (
+          <>
+            <BsPersonPlusFill />
+            <span>Add New Employee</span>
+          </>
+        )}
+      </button>
+
       <AddEmployee isFormOpen={isFormOpen} setIsFormOpen={setIsFormOpen} />
-      <EmployeeList />
+      {/* <EmployeeList /> */}
     </main>
   );
 }
--- components/AddEmployee.js
+++ components/AddEmployee.js
@@ -1,75 +1,71 @@
-export default function AddEmployee({isFormOpen, setIsFormOpen}) {
+export default function AddEmployee({ isFormOpen, setIsFormOpen }) {
     return (
-        <div
-            id="add"
-            className="w3-animate-left w3-padding w3-margin"
-            style={{ display: "none" }}
-        >
-            <div className="w3-container w3-third">
-                <form action="insert.php" method="post" autoComplete="off">
-                    <label>Name</label>
-                    <input type="text" name="name" className="w3-input w3-border" />
-                    <br />
+        <>
+            {
+                isFormOpen &&
+                <div
+                    className="w3-animate-left w3-padding w3-margin"
 
-                    <label>Address</label>
-                    <input type="text" name="address" className="w3-input w3-border" />
-                    <br />
+                >
+                    <div className="w3-container w3-third">
+                        <form action="insert.php" method="post" autoComplete="off">
+                            <label>Name</label>
+                            <input type="text" name="name" className="w3-input w3-border" />
+    
 
-                    <label>Phone Number</label>
-                    <input type="tel" name="phone" className="w3-input w3-border" />
-                    <br />
+                            <label>Joining Date</label>
+                            <input type="date" name="joining" className="w3-input w3-border" />
+           
 
-                    <label>Joining Date</label>
-                    <input type="date" name="joining" className="w3-input w3-border" />
-                    <br />
+                            <label>Designation</label>
+                            <input
+                                type="text"
+                                name="designation"
+                                className="w3-input w3-border"
+                            />
+ 
 
-                    <label>Designation</label>
-                    <input
-                        type="text"
-                        name="designation"
-                        className="w3-input w3-border"
-                    />
-                    <br />
+                            <label>Grade</label>
+                            <select name="grade" className="w3-input">
+                                <option value="A">A</option>
+                                <option value="B">B</option>
+                                <option value="C">C</option>
+                                <option value="D">D</option>
+                                <option value="E">E</option>
+                            </select>
 
-                    <label>Grade</label>
-                    <select name="grade" className="w3-input">
-                        <option value="A">A</option>
-                        <option value="B">B</option>
-                        <option value="C">C</option>
-                        <option value="D">D</option>
-                        <option value="E">E</option>
-                    </select>
-                    <br />
 
-                    <label>Loan</label>
-                    <input type="number" name="loan" className="w3-input w3-border" />
-                    <br />
+                            <label>Loan</label>
+                            <input type="number" name="loan" className="w3-input w3-border" />
 
-                    <label>Basic Salary</label>
-                    <input type="number" name="basic" className="w3-input w3-border" />
-                    <br />
 
-                    <label>House Allowance</label>
-                    <input
-                        type="number"
-                        name="allowance"
-                        className="w3-input w3-border"
-                    />
-                    <br />
+                            <label>Basic Salary</label>
+                            <input type="number" name="basic" className="w3-input w3-border" />
+            
 
-                    <button type="submit" className="w3-button w3-border w3-green">
-                        Submit
-                    </button>
+                            <label>House Allowance</label>
+                            <input
+                                type="number"
+                                name="allowance"
+                                className="w3-input w3-border"
+                            />
+                
 
-                    <button
-                        type="button"
-                        // onClick="document.getElementById('add').style.display='none'"
-                        className="w3-button w3-red"
-                    >
-                        Close
-                    </button>
-                </form>
-            </div>
-        </div>
+                            <button type="submit" className="w3-button w3-border w3-green">
+                                Submit
+                            </button>
+
+                            <button
+                                type="button"
+                                // onClick="document.getElementById('add').style.display='none'"
+                                className="w3-button w3-red"
+                            >
+                                Close
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            }
+        </>
     )
 }
