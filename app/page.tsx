caafa1d792c39772e8c89e6754f5cc8eb961eb8c
Al Sajid <alsajiduzzaman@outlook.com>
2023-05-19 20:19:26 +0600

    Home page skeleton added

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,132 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <>
+      <h1 className="w3-center">Payroll Management System</h1>
+      <hr className="w3-margin-left w3-margin-right" />
+
+      <button
+        // onClick="document.getElementById('add').style.display='block'"
+        className="w3-button w3-green w3-margin w3-padding"
+      >
+        Add New Employee
+      </button>
+      <br />
+      <br />
+      <br />
+
+      <div
+        id="add"
+        className="w3-animate-left w3-padding w3-margin"
+        style={{ display: "none" }}
+      >
+        <div className="w3-container w3-third">
+          <form action="insert.php" method="post" autoComplete="off">
+            <label>Name</label>
+            <input type="text" name="name" className="w3-input w3-border" />
+            <br />
+
+            <label>Address</label>
+            <input type="text" name="address" className="w3-input w3-border" />
+            <br />
+
+            <label>Phone Number</label>
+            <input type="tel" name="phone" className="w3-input w3-border" />
+            <br />
+
+            <label>Joining Date</label>
+            <input type="date" name="joining" className="w3-input w3-border" />
+            <br />
+
+            <label>Designation</label>
+            <input
+              type="text"
+              name="designation"
+              className="w3-input w3-border"
             />
-          </a>
+            <br />
+
+            <label>Grade</label>
+            <select name="grade" className="w3-input">
+              <option value="A">A</option>
+              <option value="B">B</option>
+              <option value="C">C</option>
+              <option value="D">D</option>
+              <option value="E">E</option>
+            </select>
+            <br />
+
+            <label>Loan</label>
+            <input type="number" name="loan" className="w3-input w3-border" />
+            <br />
+
+            <label>Basic Salary</label>
+            <input type="number" name="basic" className="w3-input w3-border" />
+            <br />
+
+            <label>House Allowance</label>
+            <input
+              type="number"
+              name="allowance"
+              className="w3-input w3-border"
+            />
+            <br />
+
+            <button type="submit" className="w3-button w3-border w3-green">
+              Submit
+            </button>
+
+            <button
+              type="button"
+              // onClick="document.getElementById('add').style.display='none'"
+              className="w3-button w3-red"
+            >
+              Close
+            </button>
+          </form>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div>
+        <table
+          className="w3-table w3-striped w3-border w3-bordered w3-centered w3-hoverable w3-table-all"
+          id="record"
+        >
+          <tr>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Phone number</th>
+            <th>Joining date</th>
+            <th>Designation</th>
+            <th>Grade</th>
+            <th>Loan</th>
+            <th>Basic</th>
+            <th>House Allowance</th>
+            <th colSpan={3}>Action</th>
+          </tr>
+          <tr>
+            <td>name</td>
+            <td>address</td>
+            <td>phone number</td>
+            <td>joining date</td>
+            <td>designation</td>
+            <td>grade</td>
+            <td>loan</td>
+            <td>basic</td>
+            <td>house allowance</td>;
+            <td className="w3-blue">
+              <a href='slip.php?code=" . $row["code"] . "'>Pay</a>
+            </td>
+            <td className="w3-yellow">
+              <a href='modify.php?code=" . $row["code"] . "'>Modify</a>
+            </td>
+            <td className="w3-red">
+              <a href='delete.php?code=" . $row["code"] . "'>Delete</a>
+            </td>
+          </tr>
+        </table>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </>
+  );
 }
